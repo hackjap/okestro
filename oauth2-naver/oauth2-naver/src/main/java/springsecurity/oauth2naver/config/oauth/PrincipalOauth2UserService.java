275931a0093d60dc18b36ae94b40d9032ce7c9f8
hackjap <hackjap1015@gmail.com>
2021-08-17 14:13:30 +0900

    :sparkles:페이스북 로그인

--- oauth2-naver/oauth2-naver/src/main/java/springsecurity/oauth2naver/config/oauth/PrincipalOauth2UserService.java
+++ oauth2-naver/oauth2-naver/src/main/java/springsecurity/oauth2naver/config/oauth/PrincipalOauth2UserService.java
@@ -8,6 +8,9 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import springsecurity.oauth2naver.config.auth.PrincipalDetails;
+import springsecurity.oauth2naver.config.oauth.provider.FacebookUserInfo;
+import springsecurity.oauth2naver.config.oauth.provider.GoogleUserInfo;
+import springsecurity.oauth2naver.config.oauth.provider.OAuth2UserInfo;
 import springsecurity.oauth2naver.model.User;
 import springsecurity.oauth2naver.repository.UserRepository;
 
@@ -24,19 +27,33 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     // 함수 종료시 @AuthenticationPrincipal 어노테이션이 만들어진다.
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        System.out.println("userRequest = " + userRequest.getClientRegistration()); // registrationId로 어떤 OAuth로 로그인 했는지 확인 가능.
-        System.out.println("userRequest = " + userRequest.getAccessToken().getTokenValue());
+        System.out.println("getClientRegistration = " + userRequest.getClientRegistration()); // registrationId로 어떤 OAuth로 로그인 했는지 확인 가능.
+        System.out.println("getAccessToken = " + userRequest.getAccessToken().getTokenValue());
 
         OAuth2User oAuth2User =  super.loadUser(userRequest);
         // 구글로그인 버튼 클릭 -> 구글로그인창 -> 로그인 완료 -> code를 리턴 (OAuth -Client 라이브러리) -> AccessToken 요청
         // userRquest 정보 -> loadUser함수 호출 -> 구글로부터 회원프로필 받아준다.
-        System.out.println("userRequest = " + oAuth2User.getAttributes());
+        System.out.println("getAttributes = " + oAuth2User.getAttributes());
 
-        String provider = userRequest.getClientRegistration().getClientId(); // google
-        String providerId = oAuth2User.getAttribute("sub");
+        // 회원가입을 강제로 진행해볼 예정
+        OAuth2UserInfo oAuth2UserInfo = null;
+        if(userRequest.getClientRegistration().getRegistrationId().equals("google")){
+            System.out.println("구글 로그인 요청");
+            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
+            System.out.println("페이스북 로그인 요청");
+            oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+
+        }else{
+            System.out.println("구글과 페이스북 로그인만 지원합니다.");
+        }
+
+
+        String provider = oAuth2UserInfo.getProvider(); // google
+        String providerId =oAuth2UserInfo.getProviderId();
         String username = provider + "_"+providerId; // google_117907683636938846420
         String password = bCryptPasswordEncoder.encode("겟인데어");
-        String email = oAuth2User.getAttribute("email");
+        String email = oAuth2UserInfo.getEmail();
         String role = "ROLE_USER";
 
         User userEntity = userRepository.findByUsername(username);
@@ -53,9 +70,9 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
                     .build();
             userRepository.save(userEntity);
         } else {
-            System.out.println("구글 로그인을 이미 한적이 있습니다. 당신은 자동회원가입이 되어 있습니다.");
+            System.out.println("로그인을 이미 한적이 있습니다. 당신은 자동 회원가입이 되어 있습니다.");
         }
-        // 회원가입을 강제로 진행해볼 예정
+
         return new PrincipalDetails(userEntity,oAuth2User.getAttributes());
 
     }
---
+++ oauth2-naver/oauth2-naver/src/main/java/springsecurity/oauth2naver/config/oauth/provider/OAuth2UserInfo.java
@@ -0,0 +1,14 @@
+package springsecurity.oauth2naver.config.oauth.provider;
+
+public interface OAuth2UserInfo {
+
+    String getProviderId();
+
+    String getProvider();
+
+    String getEmail();
+
+    String getName();
+
+
+}
