70c400cc0934348662149fe41af5e70931b9bfbf
hackjap <hackjap1015@gmail.com>
2021-08-18 15:44:28 +0900

    :sparkles:jwt 임시 토큰 만들어서 테스트 해보기

--- jwt/src/main/java/com/jwt/filter/MyFilter3.java
+++ jwt/src/main/java/com/jwt/filter/MyFilter3.java
@@ -1,7 +1,10 @@
 package com.jwt.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 public class MyFilter3 implements Filter {
@@ -9,7 +12,29 @@ public class MyFilter3 implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("필터 3");
-        filterChain.doFilter(servletRequest,servletResponse);
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+        // 토큰 : cos -> id,pw 정상적으로 들어와서 로그인이 완료되면 토큰을 만들어주고 그걸 응답해준다.
+        // 요청할 떄마다 Header에 Authorization에 value 값으로 토큰을 가지고 옴
+        // 그떄 토큰이 넘어오면 이 토큰이 내가 만든 토큰이 맞는지만 검증만 하면 됨. ( RSA , HS256 )
+
+        if (req.getMethod().equals("POST")) {
+            System.out.println("post 요청됨");
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println("headerAUth = " + headerAuth);
+
+            if (headerAuth.equals("cos")) {
+                filterChain.doFilter(req, res);
+            }else{
+                System.out.println("필터 1");
+                PrintWriter out = res.getWriter();
+                out.println("인증안됨");
+            }
+
+        }
+
+
     }
 }
